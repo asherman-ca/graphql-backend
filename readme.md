2da2d39e665e63641f7be0923b82bba4a379aeb9
Alex Sherman <asherman.ca@gmail.com>
2020-04-17 19:20:27 -0700

    wip

--- readme.md
+++ readme.md
@@ -1,1 +1,15 @@
 Prisma deploy: prisma deploy --env-file variables.env
+
+graphql playground syntax exp:
+
+query {
+  dogs {
+    name
+  }
+}
+
+mutation {
+  createDog(name: "dogname") {
+    name
+  }
+}
--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,9 @@
-const Mutations = {};
+const Mutations = {
+  createDog(parent, args, ctx, info) {
+    global.dogs = global.dogs || [];
+    global.dogs.push({name: args.name});
+    return {name: args.name};
+  }
+};
 
 module.exports = Mutations;
